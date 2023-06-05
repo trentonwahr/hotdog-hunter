c80eb0e5da14dd098886803ff83c1c9939be9328
trentonwahr <trentwahr@gmail.com>
2023-06-05 15:27:50 -0400

    Fix sign up redirect

--- main_app/views.py
+++ main_app/views.py
@@ -43,7 +43,7 @@ def signup(request):
     if form.is_valid():
       user = form.save()
       login(request, user)
-      return redirect('dog-index')
+      return redirect('hotdog-index')
     else:
       error_message = 'Invalid sign up - try again'
   form = UserCreationForm()
