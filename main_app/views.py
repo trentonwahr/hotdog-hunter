b03fbc618c8fe47026ee041223af354447ad9964
trentonwahr <trentwahr@gmail.com>
2023-06-05 09:54:29 -0400

    stub up hotdog index

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from django.http import HttpResponse
+
+class Home(LoginView):
+  template_name = 'home.html'
+
+def about(request):
+  return render(request, 'about.html')
+
+class HotdogList(ListView):
+  model = Hotdog
