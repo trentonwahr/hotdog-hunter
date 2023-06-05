6e54258aaa6876d097989176cfa8b5c6219a7071
trentonwahr <trentwahr@gmail.com>
2023-06-05 15:14:06 -0400

    add log in

--- main_app/models.py
+++ main_app/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 class Hotdog(models.Model):
   restaurant = models.CharField(max_length=100)
   location = models.CharField(max_length=100)
   description = models.TextField(max_length=300)
   rating = models.IntegerField(choices=list(zip(range(1,6), range(1,6))), default=5)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
   
   def __str__(self):
     return self.restaurant
