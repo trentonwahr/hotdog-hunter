97e7e0a8095a5b12b2dacfcf94e64eb6edc2d4fd
trentonwahr <trentwahr@gmail.com>
2023-06-05 15:16:39 -0400

    update creat function

--- main_app/views.py
+++ main_app/views.py
@@ -24,6 +24,10 @@ class HotdogCreate(CreateView):
   model = Hotdog
   fields = ['restaurant', 'location', 'rating', 'description']
 
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+
 class HotdogUpdate(UpdateView):
   model = Hotdog
   fields = ['location', 'rating', 'description']
